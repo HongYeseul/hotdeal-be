d7df156756697e13fc3ddcdf6a3aebd580c28893
HongYeseul <ghd8119@naver.com>
2024-10-06 23:36:49 +0900

    feat: redis-kafka 로직 분리

--- src/main/java/com/example/hot_deal/order/service/OrderService.java
+++ src/main/java/com/example/hot_deal/order/service/OrderService.java
@@ -1,42 +1,27 @@
 package com.example.hot_deal.order.service;
 
-import com.example.hot_deal.order.domain.entity.Order;
-import com.example.hot_deal.order.domain.repository.OrderRepository;
+import com.example.hot_deal.order.domain.repository.PurchasedUserRepository;
 import com.example.hot_deal.product.domain.entity.Product;
 import com.example.hot_deal.product.domain.repository.ProductCountRepository;
 import com.example.hot_deal.product.domain.repository.ProductRepository;
-import com.example.hot_deal.user.domain.entity.User;
-import com.example.hot_deal.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-    private final OrderRepository orderRepository;
 
     private final ProductRepository productRepository;
-    private final UserRepository userRepository;
-
     private final ProductCountRepository productCountRepository;
+    private final PurchasedUserRepository purchasedUserRepository;
 
-    @Transactional
-    public void orderProduct(Long userId, Long orderId) {
-        User user = userRepository.findById(userId).orElseThrow(RuntimeException::new);
-        Product product = productRepository.findById(orderId).orElseThrow(RuntimeException::new);
-
-        String productKey = product.getId().toString();
+    public void orderProduct(Long userId, Long productId) {
+        String productKey = productId.toString();
+        Product product = productRepository.findById(productId).orElseThrow(RuntimeException::new);
         productCountRepository.decrement(productKey, product.getStockQuantity().toString());
 
-        product.decreaseQuantity();
-        productRepository.save(product);
-
-        orderRepository.save(Order.builder()
-                .user(user)
-                .product(product)
-                .build());
+        purchasedUserRepository.purchase(userId, productId);
     }
 }
