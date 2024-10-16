1dbe68e77c782bad9371e8b820ad367e9bc9b92f
HongYeseul <ghd8119@naver.com>
2024-10-17 00:42:58 +0900

    feat: Validator 용 Regex pattern 클래스 생성

---
+++ src/main/java/com/example/hot_deal/common/util/validation/validator/ValidatePatterns.java
@@ -0,0 +1,7 @@
+package com.example.hot_deal.common.util.validation.validator;
+
+public class ValidatePatterns {
+    public static final String NAME_REGEX = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9!@#$%^&*()_+\\-=?<>]{2,6}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{10,20}$";
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+}
