dcd55a8b3f7475a631dccd7bf38840cf6d93aacf
HongYeseul <ghd8119@naver.com>
2024-10-17 00:47:40 +0900

    feat: User 용 Base DTO 생성

---
+++ src/main/java/com/example/hot_deal/user/dto/base/BaseUserDTO.java
@@ -0,0 +1,21 @@
+package com.example.hot_deal.user.dto.base;
+
+import com.example.hot_deal.common.util.validation.annotaion.ValidEmail;
+import com.example.hot_deal.common.util.validation.annotaion.ValidName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "회원 기본 정보")
+public class BaseUserDTO {
+    @ValidEmail
+    private final String email;
+
+    @ValidName
+    private final String name;
+
+    public BaseUserDTO(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+}
