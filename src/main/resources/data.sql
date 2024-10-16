aaea2b6f0f2fee3054eb6f8bbd278adaff451f54
HongYeseul <ghd8119@naver.com>
2024-10-16 20:58:38 +0900

    feat: 시작시 더미 데이터를 갖고 시작할 수 있도록 sql 문 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, price, stock_quantity, open_time)
+VALUES
+    ('Product1', 1000, 50, '2024-10-16 09:00:00'),
+    ('Product2', 5500, 100, '2024-10-16 09:00:00'),
+    ('Product3', 1500, 200, '2024-10-16 09:00:00'),
+    ('Product4', 7450, 100, '2024-10-16 09:00:00');
