5c03668f9b3c893c8d13bedad9f1b31076870a77
HongYeseul <ghd8119@naver.com>
2024-10-21 10:30:45 +0900

    feat: auth provider 추가

---
+++ src/main/java/com/example/hot_deal/auth/service/AuthProvider.java
@@ -0,0 +1,29 @@
+package com.example.hot_deal.auth.service;
+
+import com.example.hot_deal.auth.constants.TokenType;
+import com.example.hot_deal.auth.dto.MemberTokens;
+import com.example.hot_deal.member.domain.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthProvider {
+
+    private final JwtProvider jwtProvider;
+
+    public MemberTokens makeToken(Member member) {
+        return new MemberTokens(
+                createAccessToken(member),
+                createRefreshToken(member)
+        );
+    }
+
+    public String createAccessToken(Member member) {
+        return jwtProvider.createToken(member, TokenType.ACCESS_TOKEN);
+    }
+
+    public String createRefreshToken(Member member) {
+        return jwtProvider.createToken(member, TokenType.REFRESH_TOKEN);
+    }
+}
