f65c3d2d19056f0a6d4df1b6e692ab8364775dd8
HongYeseul <ghd8119@naver.com>
2024-10-21 15:00:05 +0900

    refactor: provider 패키지 생성

--- src/main/java/com/example/hot_deal/auth/service/AuthService.java
+++ src/main/java/com/example/hot_deal/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package com.example.hot_deal.auth.service;
 
 import com.example.hot_deal.auth.dto.LoginRequest;
 import com.example.hot_deal.auth.dto.MemberTokens;
+import com.example.hot_deal.auth.provider.AuthProvider;
 import com.example.hot_deal.common.exception.HotDealException;
 import com.example.hot_deal.member.domain.entity.Member;
 import com.example.hot_deal.member.domain.repository.MemberRepository;
