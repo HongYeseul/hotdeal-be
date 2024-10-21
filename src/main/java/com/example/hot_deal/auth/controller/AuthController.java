ddd13aafec30615447670420564cc78319e6895a
HongYeseul <ghd8119@naver.com>
2024-10-21 22:23:21 +0900

    fix: 로그인 메서드 수정

--- src/main/java/com/example/hot_deal/auth/controller/AuthController.java
+++ src/main/java/com/example/hot_deal/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.hot_deal.auth.dto.MemberTokens;
 import com.example.hot_deal.auth.manager.CredentialManager;
 import com.example.hot_deal.auth.service.AuthService;
 import com.example.hot_deal.common.config.web.ApiV1;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.hot_deal.auth.constants.JwtConstants.AUTHORIZATION_HEADER;
+import static com.example.hot_deal.auth.constants.JwtConstants.BEARER_TOKEN_PREFIX;
 
 @ApiV1
 @RestController
@@ -24,13 +28,22 @@ public class AuthController {
     /**
      * 로그인
      */
+//    @PostMapping("/login")
+//    public ResponseEntity<MemberTokens> login(
+//            @Valid @RequestBody LoginRequest loginRequest,
+//            HttpServletResponse httpServletResponse
+//    ) {
+//        MemberTokens memberTokens = authService.login(loginRequest);
+//        credentialManager.setCredential(memberTokens, httpServletResponse);
+//        return ResponseEntity.ok(memberTokens);
+//    }
+
     @PostMapping("/login")
-    public ResponseEntity<MemberTokens> login(
-            @Valid @RequestBody LoginRequest loginRequest,
-            HttpServletResponse httpServletResponse
-    ) {
-        MemberTokens memberTokens = authService.login(loginRequest);
-        credentialManager.setCredential(memberTokens, httpServletResponse);
-        return ResponseEntity.ok(memberTokens);
+    @Operation(summary = "로그인", description = "이메일, 비밀번호를 입력하여 로그인을 한다.")
+    public ResponseEntity<Void> login(@Valid @RequestBody final LoginRequest loginRequest, HttpServletResponse response) {
+        MemberTokens tokens = authService.login(loginRequest);
+        response.addCookie(tokens.refreshToken());
+        response.setHeader(AUTHORIZATION_HEADER, BEARER_TOKEN_PREFIX + tokens.accessToken());
+        return ResponseEntity.ok().build();
     }
 }
