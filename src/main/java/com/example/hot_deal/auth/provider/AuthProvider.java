5f7784b78a639583d9dfadd50a897dbcfb3762d4
HongYeseul <ghd8119@naver.com>
2024-10-24 17:13:42 +0900

    refactor: 메서드 네이밍 변경(create->generate)

--- src/main/java/com/example/hot_deal/auth/provider/AuthProvider.java
+++ src/main/java/com/example/hot_deal/auth/provider/AuthProvider.java
@@ -3,6 +3,7 @@ package com.example.hot_deal.auth.provider;
 import com.example.hot_deal.auth.constants.TokenType;
 import com.example.hot_deal.auth.dto.MemberTokens;
 import com.example.hot_deal.member.domain.entity.Member;
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +14,12 @@ public class AuthProvider {
     private final JwtProvider jwtProvider;
     private final CookieProvider cookieProvider;
 
-    public MemberTokens makeToken(Member member) {
-        String refreshToken = createRefreshToken(member);
-        return new MemberTokens(
-                createAccessToken(member),
-                cookieProvider.createCookieForToken(refreshToken, TokenType.REFRESH_TOKEN)
-        );
+    public MemberTokens generateToken(Member member) {
+        String accessToken = generateAccessToken(member);
+        String refreshToken = generateRefreshToken(member);
+
+        Cookie refreshTokenCookie = cookieProvider.createCookieForToken(refreshToken, TokenType.REFRESH_TOKEN);
+        return new MemberTokens(accessToken, refreshTokenCookie);
     }
 
 //    public MemberTokens makeToken(Member member) {
@@ -28,11 +29,11 @@ public class AuthProvider {
 //        );
 //    }
 
-    private String createAccessToken(Member member) {
+    private String generateAccessToken(Member member) {
         return jwtProvider.createToken(member, TokenType.ACCESS_TOKEN);
     }
 
-    private String createRefreshToken(Member member) {
+    private String generateRefreshToken(Member member) {
         return jwtProvider.createToken(member, TokenType.REFRESH_TOKEN);
     }
 }
