3039ee6af51b6ac870415fc59d4280c6c8c42328
HongYeseul <ghd8119@naver.com>
2024-10-25 11:33:29 +0900

    feat: Product List로 저장할 수 있는 메서드 추가

--- src/main/java/com/example/hot_deal/product/domain/repository/ProductRepository.java
+++ src/main/java/com/example/hot_deal/product/domain/repository/ProductRepository.java
@@ -4,6 +4,8 @@ import com.example.hot_deal.product.domain.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface ProductRepository {
 
     Product getProductById(Long id);
@@ -15,4 +17,6 @@ public interface ProductRepository {
     Product save(Product product);
 
     void deleteAll();
+
+    List<Product> saveAllProducts(List<Product> products);
 }
