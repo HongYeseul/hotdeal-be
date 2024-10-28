2feff236c130b7b153175192f00a093ac06b197d
HongYeseul <ghd8119@naver.com>
2024-10-28 23:19:50 +0900

    feat: Add Redis Config

---
+++ src/main/java/com/example/hot_deal/product/config/RedisRepositoryConfig.java
@@ -0,0 +1,9 @@
+package com.example.hot_deal.product.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(basePackages = "com.example.hot_deal.product.domain.repository")
+public class RedisRepositoryConfig {
+}
