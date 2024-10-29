dcc386f98e852f9620856ea0cef0f603485813b0
HongYeseul <ghd8119@naver.com>
2024-10-29 16:13:43 +0900

    refactor: Price 클래스 생성

---
+++ src/main/java/com/example/hot_deal/common/domain/Price.java
@@ -0,0 +1,28 @@
+package com.example.hot_deal.common.domain;
+
+import com.example.hot_deal.common.exception.HotDealException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+import static com.example.hot_deal.common.exception.code.CommonErrorCode.INVALID_PRICE;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Price {
+
+    @Column(nullable = false)
+    BigDecimal price;
+
+    public Price(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new HotDealException(INVALID_PRICE);
+        }
+        this.price = price;
+    }
+}
