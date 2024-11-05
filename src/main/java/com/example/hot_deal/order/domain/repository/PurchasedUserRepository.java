36f64c42e6c2ffe6535de0344bdc5382a37476d9
HongYeseul <ghd8119@naver.com>
2024-11-05 10:59:16 +0900

    refactor(Kafka): 요청 Javadoc 주석 추가

--- src/main/java/com/example/hot_deal/order/domain/repository/PurchasedUserRepository.java
+++ src/main/java/com/example/hot_deal/order/domain/repository/PurchasedUserRepository.java
@@ -12,6 +12,11 @@ public class PurchasedUserRepository {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    /**
+     * 구매 요청 Topic 에 메시지 전달
+     * @param userId 구매한 회원
+     * @param productId 구매 제품
+     */
     public void purchase(Long userId, Long productId) {
         String key = userId.toString();
         String value = userId + ":" + productId;
