e1a6fc214ac7854bd9f7f41eb383446e0c5d3148
HongYeseul <ghd8119@naver.com>
2024-12-05 17:02:13 +0900

    comment: ApiV1 클래스 주석 추가

--- src/main/java/com/example/hot_deal/common/config/web/ApiV1.java
+++ src/main/java/com/example/hot_deal/common/config/web/ApiV1.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *  ElementType.TYPE: 클래스, 인터페이스, enum에서 사용할 수 있는 애노테이션
+ */
 @Target(ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 @Component
